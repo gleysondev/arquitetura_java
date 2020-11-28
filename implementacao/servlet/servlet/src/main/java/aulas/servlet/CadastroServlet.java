fbccd5dcaa3e36a25a76a288ae7116f58dc18c5d
Gleyson Sampaio <gleysondev@gmail.com>
2020-11-28 15:40:09 -0300

    formulario servlet

---
+++ implementacao/servlet/servlet/src/main/java/aulas/servlet/CadastroServlet.java
@@ -0,0 +1,48 @@
+package aulas.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PrimeiraServlet
+ */
+public class CadastroServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CadastroServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");  
+		PrintWriter out = response.getWriter();  
+		          
+		String nome=request.getParameter("nome");  
+		String posto=request.getParameter("posto");
+		
+		
+		out.print(String.format("O Oficial %s %s cadastrado com sucesso", nome, posto));  
+		out.close();
+	}
+
+}
