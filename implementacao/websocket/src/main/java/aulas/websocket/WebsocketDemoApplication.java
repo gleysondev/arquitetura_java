9d919698b6028b58ae636b954ea4f9428e84568c
Gleyson Sampaio <gleysondev@gmail.com>
2020-12-01 07:37:20 -0300

    Update WebsocketDemoApplication.java

--- implementacao/websocket/src/main/java/aulas/websocket/WebsocketDemoApplication.java
+++ implementacao/websocket/src/main/java/aulas/websocket/WebsocketDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebsocketDemoApplication {
-
+	//https://www.devmedia.com.br/jsonp-api-como-processar-json-com-java-ee-7/29901
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketDemoApplication.class, args);
 	}
