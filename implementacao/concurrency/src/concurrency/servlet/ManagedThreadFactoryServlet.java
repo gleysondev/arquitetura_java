b8f5eb0e383a9869276afc4ed21234763f3dbac3
Gleyson Sampaio <gleysondev@gmail.com>
2020-12-03 00:17:46 -0300

    Update ManagedThreadFactoryServlet.java

--- implementacao/concurrency/src/concurrency/servlet/ManagedThreadFactoryServlet.java
+++ implementacao/concurrency/src/concurrency/servlet/ManagedThreadFactoryServlet.java
@@ -8,17 +8,18 @@ import javax.annotation.Resource;
 import javax.enterprise.concurrent.ManagedThreadFactory;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import concurrency.task.RunnableTask;
 
 //http://localhost:8080/concurrency/MTFServlet
-@WebServlet("/MTFServlet")
-public class ManagedThreadFactoryServlet {
+//@WebServlet("/MTFServlet")
+public class ManagedThreadFactoryServlet extends HttpServlet {
 	private static final long serialVersionUID = -7366024900440622521L;
 
-	 @Resource(name = "java:jboss/ee/concurrency/factory/Aula_MTF")
+	// @Resource(name = "java:jboss/ee/concurrency/factory/Aula_MTF")
 	 private ManagedThreadFactory managedThreadFactory;
 
 	 private static final Logger LOG =
