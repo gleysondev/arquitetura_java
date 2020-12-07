082e72ea0be13b2bae57d8854463c60318646a34
Gleyson Sampaio <gleysondev@gmail.com>
2020-12-07 19:15:22 -0300

    spring security

---
+++ implementacao/spring/spring-security/src/main/java/aulas/config/WebSecurity.java
@@ -0,0 +1,22 @@
+package aulas.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception { 
+        http
+        .cors().and()
+        .csrf().disable().authorizeRequests()
+        
+        .antMatchers("/home").permitAll()
+        .antMatchers("/admin").hasRole("manager")
+        
+        .anyRequest().authenticated()
+        .and()
+        .formLogin();
+    }
+}
