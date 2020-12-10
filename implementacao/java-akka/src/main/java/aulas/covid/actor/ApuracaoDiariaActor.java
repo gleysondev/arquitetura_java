faec08b8fe2e99b6638339e403715f4ee06ccd80
Gleyson Sampaio <gleysondev@gmail.com>
2020-12-10 20:54:15 -0300

    exemplo final de actores

--- implementacao/java-akka/src/main/java/aulas/covid/actor/ApuracaoDiariaActor.java
+++ implementacao/java-akka/src/main/java/aulas/covid/actor/ApuracaoDiariaActor.java
@@ -17,16 +17,25 @@ public class ApuracaoDiariaActor extends UntypedActor{
 		
 		List<ApuracaoDiaria> apuracoes = a.getApuracoes();
 		for(ApuracaoDiaria ad: apuracoes) {
+			if(a.getEstado().equals(Estado.MARANHAO.getSigla())) {
+				System.out.println(String.format("%s Casos %d - Novos Casos %d ", a.getEstado(), a.getCasos(),ad.getCasos()));
+				
+			}
 			a.apurarCasos(ad.getCasos());
 			a.apurarMortes(ad.getMortes());
 			a.apurarRecuperados(ad.getRecuperados());
+			
+			
 		}
+		System.out.println(a);
+		/*
 		if(a.getEstado().equals(Estado.SAO_PAULO.getSigla())) {
 			System.out.println(a);
 		}
 		else {
 			System.out.println(a.getEstado());
 		}
+		*/
 	}
 
 }
